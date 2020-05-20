8969cd7fcdf58f234641d11d701a04083940d6c1
tadtapong <tadtapong2549@gmail.com>
2020-05-20 11:32:27 +0700

    projectexercise4

---
+++ assignments/Exercise4_Tadtapong_C.py
@@ -0,0 +1,10 @@
+fe = 90
+gb = 90.5
+itcs = 95
+cp = 100
+
+print("--- Your Score ---")
+print("Foundation English :", fe)
+print("General Business :", gb)
+print("Introduction to Computer Systems :", itcs)
+print("Computer Programming :", cp)
